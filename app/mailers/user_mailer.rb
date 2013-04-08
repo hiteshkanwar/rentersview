bd8b18f2e2b42466d7e2ba0d42d280b93311e642
HTP <htp@htp-desktop.(none)>
2013-04-08 15:54:31 +0530

    refs to mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,9 @@ class UserMailer < ActionMailer::Base
   default from: "from@example.com"
   def welcome_user(message,ad)
     @user = message.receiver.first_name
-    subject = message.sender.name + ' sent you a message for your ad'
-    @message = subject + ":<br/> "+ ad.message + "<br/>" + message.content
-    mail(:to => message.receiver.email, :subject => subject)
+    @subject = message.sender.name + ' sent you a message for your ad'
+    @ad_message = ad.message
+    @message = message.content
+    mail(:to => message.receiver.email, :subject => @subject)
   end
 end
