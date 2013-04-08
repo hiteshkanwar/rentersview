e218e8bbad926435417b80155406e170de2a143e
HTP <htp@htp-desktop.(none)>
2013-04-08 13:08:30 +0530

    refs to migration

--- db/migrate/20130406090533_add_imageable_to_location_photo.rb
+++ db/migrate/20130406090533_add_imageable_to_location_photo.rb
@@ -1,6 +1,5 @@
 class AddImageableToLocationPhoto < ActiveRecord::Migration
   def change
-  	 add_column :location_photos, :location_id, :integer
   	 add_column :location_photos, :imageable_id,:integer
      add_column :location_photos,:imageable_type,:string
 
