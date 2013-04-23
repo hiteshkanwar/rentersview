3b89e9cf90750555af3999e626e09601ee1f9167
HTP <htp@htp-desktop.(none)>
2013-04-23 17:05:20 +0530

    refs to changes

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,9 +1,11 @@
 class Ad < ActiveRecord::Base
-  attr_accessible :message, :user_id, :location_id, :contact, :suburb, :address,:location_photos
+  attr_accessible :message, :user_id, :location_id, :contact, :suburb, :address,:location_photos,:terms_of_service
 
   validates :contact, :presence => true
 
   belongs_to :location
   belongs_to :user
   has_many :location_photos, :as => :imageable
+
+  validates :terms_of_service, :acceptance => true
 end
