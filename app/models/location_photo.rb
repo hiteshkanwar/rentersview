dcffe0ea033698e1cff075fd7de62c50c7fe11b7
HTP <htp@htp-desktop.(none)>
2013-04-26 14:31:43 +0530

    refs to amazon s3 changes

--- app/models/location_photo.rb
+++ app/models/location_photo.rb
@@ -6,13 +6,17 @@ class LocationPhoto < ActiveRecord::Base
   
   has_attached_file :photo, 
                     :styles => {
-		                    	:large => "800x748>",
-		                    	:medium => "300x248>",
-		                    	:thumb => "90x78>",
-                                :storage => :s3,
-                                :s3_credentials => "#{Rails.root}/config/s3.yml",
-                                :path => "/:style/:id/:filename"
-		                    }
+                      :large => "800x748>",
+                      :medium => "300x248>",
+                      :thumb => "90x78>"
+                    },
+                    :storage => :s3,
+                    :s3_credentials => {
+                      :bucket => 'rentersviewnew',
+                      :access_key_id => "AKIAJQQNROVXNSD4CVVQ",
+                      :secret_access_key => "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"
+                    },
+                    :path => "/:style/:id/:filename"
                     
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
