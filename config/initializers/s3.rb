367b2e543cd9380a64d374def9ea339cec5ff071
HTP <htp@htp-desktop.(none)>
2013-04-26 15:02:52 +0530

    sss

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,7 +1,7 @@
 # initializers/s3.rb
 if Rails.env == "production"
   # set credentials from ENV hash
-  S3_CREDENTIALS = { :access_key_id => "AKIAJQQNROVXNSD4CVVQ", :secret_access_key => "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", :bucket => "rentersviewnew"}
+   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
 else
   # get credentials from YML file
   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
