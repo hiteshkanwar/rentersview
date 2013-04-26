8d792ca3e7f279c7f169999b820912c4055cd0dd
HTP <htp@htp-desktop.(none)>
2013-04-26 14:51:52 +0530

    refs to amazon s3 changes

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,7 +1,7 @@
 # initializers/s3.rb
 if Rails.env == "production"
   # set credentials from ENV hash
-  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "rentersview"}
+  S3_CREDENTIALS = { :access_key_id => "AKIAJQQNROVXNSD4CVVQ", :secret_access_key => "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", :bucket => "rentersviewnew"}
 else
   # get credentials from YML file
   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
